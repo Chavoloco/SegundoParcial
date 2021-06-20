2c7f15208bad32950517686e60e95ba21dfa1b04
Chavoloco <58275860+Chavoloco@users.noreply.github.com>
2021-06-19 22:53:22 -0300

    aa

--- enviroment/CrudClientes/clientes/urls.py
+++ enviroment/CrudClientes/clientes/urls.py
@@ -5,4 +5,15 @@ from . import views
 urlpatterns = [
     url(r"^$", views.index, name="index"),
     url(r"^clientes_list/$", views.listaClientes.as_view(), name="clientes_list"),
+    url(r"^cliente/create/$", views.CrearCliente.as_view(), name="cliente_create"),
+    url(
+        r"^cliente/(?P<pk>\d+)/update/$",
+        views.ActualizarCliente.as_view(),
+        name="cliente_update",
+    ),
+    url(
+        r"^cliente/(?P<pk>\d+)/delete/$",
+        views.EliminarCliente.as_view(),
+        name="cliente_delete",
+    ),
 ]
--- enviroment/CrudClientes/clientes/views.py
+++ enviroment/CrudClientes/clientes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from clientes.models import Clientes
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+# from .models import Clientes
 
 
 def index(request):
@@ -29,9 +33,31 @@ def index(request):
 
 class listaClientes(generic.ListView):
     model = Clientes
-    paginate_by = 5
+    paginate_by = 10
 
     # def get_queryset(self):
     #     return Clientes.objects.filter(nombre__icontains="jer")[
     #         :5
     #     ]  # Get 5 clientes containing the name jer
+
+
+class CrearCliente(CreateView):
+    model = Clientes
+    fields = "__all__"
+    success_url = "/clientes/clientes_list/"
+    # initial = {
+    #     "date_of_death": "05/01/2018",
+    # }
+
+
+class ActualizarCliente(UpdateView):
+    model = Clientes
+    # fields = ["nombre", "apellido", "pais", "telefono", "direccion", "dni"]
+    fields = "__all__"
+    success_url = "/clientes/clientes_list/"
+
+
+class EliminarCliente(DeleteView):
+    model = Clientes
+    success_url = "/clientes/clientes_list/"
+    # success_url = reverse_lazy("cliente")
